f1e69efbb23d098ef138671a657c5161f0c2ee4b
Her Fri <herbert.frichert@outlook.de>
2023-11-25 21:07:50 +0100

    add Platform model, add platform filtering for Games

--- products/models.py
+++ products/models.py
@@ -26,7 +26,8 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     genre = models.CharField(max_length=100, choices=GENRES, null=True, blank=True)
-    platform = models.CharField(max_length=100, choices=PLATFORMS, null=True, blank=True)
+    platform = models.ForeignKey('Platform', null=True, blank=True, on_delete=models.SET_NULL)
+    #platform = models.CharField(max_length=100, choices=PLATFORMS, null=True, blank=True)
     year = models.IntegerField(choices=YEARS, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -34,6 +35,13 @@ class Product(models.Model):
     #rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Platform(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q  # OR logic
-from .models import Product
+from .models import Product, Platform
 
 # Create your views here.
 
@@ -10,20 +10,28 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    platform = None
 
     if request.GET:
+        if 'platform' in request.GET:
+            platforms = request.GET['platform'].split(',')
+            products = products.filter(platform__name__in=platforms)
+            platforms = Platform.objects.filter(name__in=platforms)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(platform__icontains=query) | Q(genre__icontains=query) # i = makes queries case insensitive
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(genre__icontains=query) | Q(platform__name__icontains=query) # i = makes queries case insensitive
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_platforms': platforms,
     }
 
     
