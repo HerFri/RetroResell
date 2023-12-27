764ded9521f0704390ec78b78156badabe3911b2
Her Fri <herbert.frichert@outlook.de>
2023-12-27 16:44:10 +0100

    edit EMAIL_HOST_PASSWORD

--- retroresell/settings.py
+++ retroresell/settings.py
@@ -206,7 +206,7 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')     # env.py
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS') # env.py
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # env.py
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Default primary key field type
