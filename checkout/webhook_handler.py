72ae8621129b546b319af578114fa00ad30498be
Her Fri <herbert.frichert@outlook.de>
2024-01-26 00:46:38 +0100

    edit webhook_handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -73,7 +73,7 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country,
                 profile.default_postcode = shipping_details.address.postal_code,
                 profile.default_town_or_city = shipping_details.address.city,
-                profile.default_street_address1 =shipping_details.address.line1,
+                profile.default_street_address1 = shipping_details.address.line1,
                 profile.default_street_address2 = shipping_details.address.line2,
                 profile.save()
 
@@ -90,7 +90,6 @@ class StripeWH_Handler:
                     town_or_city__iexact=shipping_details.address.city,
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
-                    #county__iexact=shipping_details.state,
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
@@ -108,21 +107,20 @@ class StripeWH_Handler:
         else:
             order = None
             try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    user_profile=profile,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
                 for item_id, item_data in json.loads(bag).items():
-                    order = Order.objects.create(
-                        full_name=shipping_details.name,
-                        user_profile=profile,
-                        email=billing_details.email,
-                        phone_number=shipping_details.phone,
-                        country=shipping_details.address.country,
-                        postcode=shipping_details.address.postal_code,
-                        town_or_city=shipping_details.address.city,
-                        street_address1=shipping_details.address.line1,
-                        street_address2=shipping_details.address.line2,
-                        #county=shipping_details.state,
-                        original_bag=bag,
-                        stripe_pid=pid,
-                    )
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
@@ -131,6 +129,16 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items(
+                        ):
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
