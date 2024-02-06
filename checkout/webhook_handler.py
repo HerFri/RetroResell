707c043ab7c69b7c6c7f7df21d79d9e736ebba05
Her Fri <herbert.frichert@outlook.de>
2024-02-06 16:19:48 +0100

    delete commas in condition in webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -69,11 +69,11 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.default_phone_number = shipping_details.phone,
-                profile.default_postcode = shipping_details.address.postal_code,
-                profile.default_town_or_city = shipping_details.address.city,
-                profile.default_street_address1 = shipping_details.address.line1,
-                profile.default_street_address2 = shipping_details.address.line2,
+                profile.default_phone_number = shipping_details.phone
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
                 profile.save()
 
         order_exists = False
